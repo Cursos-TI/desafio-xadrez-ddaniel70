1dbdf045e59095c1e3dd11710765fceeffeb3f05
ddaniel70 <danielpereira167o@gmail.com>
2025-05-26 17:12:19 +0000

    criei o for, while e do-while, para a torre, bispo, rainha

--- xadrez.c
+++ xadrez.c
@@ -1,22 +1,64 @@
 #include <stdio.h>
-
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
-
+    // Desafio de Xadrez - MateCheck
+    // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
+    // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+         
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    // usaremos funções for para a torre 5 casas para a direita
+    // usaremos funções while para bispo. 5 casas a para cima e dereira
+    // usaremos funções do-while para rainha 8 casas para a esquerda
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    // variaveis
+    int casasbispo = 1, casasrainha = 1, casastorre;  // variaveis que determinam as casas de cada peça, incializei as variáveis com 1 para nao ocorrer possiveis erros
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    // menu de inicializaçã
+    printf("Bem Vindo Ao Jogo Xadrez revulocionário !! \n");
 
+   
+    // torre
+    printf("\nMovimentos Torre: \n");
+    
+    //uso do for para os movimentos da torre 5 casas a direita 
+
+    for (casastorre = 1; casastorre <= 5; casastorre++)
+    {
+        printf("Torre: Direita\n");
+    }
+
+
+
+    // bispo
+    printf("\nMovimentos Bispo: \n");
+    
+    // uso do while para movimentos do bispo 5 casas para cima e direita
+
+    while (casasbispo <= 5)
+    {
+        printf("Bispo: Cima, Direita\n");
+        casasbispo++ ;             // incrementação da variável
+    }
+
+
+    
+    // rainha 
+    printf("\nMovimentos Rainha: \n");
+
+    // do while para movimetnoos da rainha 8 casas a esquerda
+
+    do
+    {
+        printf("Rainha: Esquerda\n");
+        casasrainha++ ; // incrementar a variável
+
+    } while (casasrainha <= 8);
+   
+    
+    // agradecer por ter jogado
+    printf("\nObrigado Por jogar Xadrez Revulocionário !! \n");
+    
+return 0;
+}
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
@@ -28,5 +70,4 @@ int main() {
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
 
-    return 0;
-}
+
